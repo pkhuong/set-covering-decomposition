edfa17c9b0f8274a2d326d7bd72b7d522653d118
Paul Khuong <pvk@pvk.ca>
2019-06-05 17:35:50 -0400

    random-set-cover-flags: remove period from descriptions, they are printed in parentheses

--- random-set-cover-flags.cc
+++ random-set-cover-flags.cc
@@ -3,21 +3,21 @@
 #include "absl/flags/flag.h"
 
 ABSL_FLAG(double, feas_eps, 5e-3,
-          "Allowed error for the clone linking constraints.");
+          "Allowed error for the clone linking constraints");
 
-ABSL_FLAG(size_t, num_sets, 1000 * 1000, "Number of sets to pick from.");
+ABSL_FLAG(size_t, num_sets, 1000 * 1000, "Number of sets to pick from");
 
-ABSL_FLAG(size_t, num_values, 10000, "Number of values to cover.");
+ABSL_FLAG(size_t, num_values, 10000, "Number of values to cover");
 
 ABSL_FLAG(size_t, max_set_per_value, 2100,
-          "Maximum number of set that may cover any value.");
+          "Maximum number of set that may cover any value");
 
 ABSL_FLAG(size_t, min_set_per_value, 1,
-          "Minimum number of set that may cover any value.");
+          "Minimum number of set that may cover any value");
 
-ABSL_FLAG(size_t, max_iter, 100000, "Iteration limit.");
+ABSL_FLAG(size_t, max_iter, 100000, "Iteration limit");
 
 ABSL_FLAG(bool, check_feasible, false,
           "Whether the solver should look for and return solutions to "
           "relaxations that happen to be feasible and optimal for the "
-          "original problem.");
+          "original problem");
