72d42bb41c60b337ca75f8614cb7b533253c05b1
Paul Khuong <pvk@pvk.ca>
2019-06-05 16:37:22 -0400

    dummy-visualizer: style

--- dummy-visualizer.c
+++ dummy-visualizer.c
@@ -2,7 +2,9 @@
 
 int main() {
   fprintf(stderr, "Build with --define gui=yes to enable visualisation.\n");
-  fprintf(stderr, "The :visualizer target will then depend on glfw "
-          "(sudo apt-get install libglfw3{,-dev} or libglfw3{-wayland,-dev}).\n");
+  fprintf(stderr,
+          "The :visualizer target will then depend on glfw "
+          "(sudo apt-get install libglfw3{,-dev} or "
+          "libglfw3{-wayland,-dev}).\n");
   return 0;
 }
