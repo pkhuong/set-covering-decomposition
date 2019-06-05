f683a064e58f9ca0db16f477c3876181cca6117c
Paul Khuong <pvk@pvk.ca>
2019-06-05 15:26:11 -0400

    dummy-visualizer: document dependency on libglfw here

--- dummy-visualizer.c
+++ dummy-visualizer.c
@@ -2,5 +2,7 @@
 
 int main() {
   fprintf(stderr, "Build with --define gui=yes to enable visualisation.\n");
+  fprintf(stderr, "The :visualizer target will then depend on glfw "
+          "(sudo apt-get install libglfw3{,-dev} or libglfw3{-wayland,-dev}).\n");
   return 0;
 }
