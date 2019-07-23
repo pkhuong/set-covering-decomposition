a846daccfcbd041acb561826ae34d9a16aafe575
Paul Khuong <pvk@pvk.ca>
2019-07-23 14:09:44 -0400

    bench/compare-functions: internal late-bound refactor
    When we use this be benchmarking harness for regression testing, we'll
    want to easily compare with functions at specific commits.  Copying
    source, etc., is bound to be error-prone.  Instead, we'll generate
    hermetic shared objects and extract one symbol from the old and new
    .so files.
    
    When we do that, we'll have to take a late-binding hit.  The harness
    should make sure to absorb that hit outside the timed call by
    including the full preparation / conversion and timing code in the
    indirect callee.
    
    The refactor in this commit is purely internal, and is minimal to help
    bisection if we discover that late binding seems to introduce noise.
    
    TESTED=existing tests pass.

--- bench/compare-functions.h
+++ bench/compare-functions.h
@@ -200,35 +200,47 @@ struct Accumulator {
   std::vector<std::vector<Result>> buffers GUARDED_BY(mu);
 };
 
-template <typename Generator, typename PrepA, typename FnA, typename PrepB,
-          typename FnB, typename Comparator>
+// This pattern matching template checks that `T` is a
+// `std::tuple<uint64_t, uint64_t, std::tuple<...>>`, as we expect
+// from timing functions.
+//
+// When that is the case, `ResultTuple` is the last tuple last,
+// prefixed with one uint64_t (the cycle timing).
+template <typename T>
+struct IsValidResult {
+  static constexpr bool value = false;
+};
+
+template <typename... Ts>
+struct IsValidResult<std::tuple<uint64_t, uint64_t, std::tuple<Ts...>>> {
+  static constexpr bool value = true;
+
+  using ResultTuple = std::tuple<uint64_t, Ts...>;
+};
+
+template <typename Generator, typename FnA, typename FnB, typename Comparator>
 class StatisticGenerator {
   using GenResult =
       decltype(CallAndTuplify()(std::declval<Generator>(), std::make_tuple()));
-  using PrepAResult = decltype(
-      CallAndTuplify()(std::declval<PrepA>(), std::declval<const GenResult>()));
   using FnAResult = decltype(
-      CallAndTuplify()(std::declval<FnA>(), std::declval<PrepAResult>()));
-  using PrepBResult = decltype(
-      CallAndTuplify()(std::declval<PrepB>(), std::declval<const GenResult>()));
+      CallAndTuplify()(std::declval<FnA>(), std::tuple<const GenResult*>()));
   using FnBResult = decltype(
-      CallAndTuplify()(std::declval<FnB>(), std::declval<PrepBResult>()));
+      CallAndTuplify()(std::declval<FnB>(), std::tuple<const GenResult*>()));
+
+  static_assert(IsValidResult<FnAResult>::value,
+                "The result of timing functions must be a tuple<uint64_t, "
+                "uint64_t, tuple<...>>.");
+  static_assert(IsValidResult<FnBResult>::value,
+                "The result of timing functions must be a tuple<uint64_t, "
+                "uint64_t, tuple<...>>.");
 
   using Result = decltype(absl::apply(
       std::declval<Comparator>(),
       std::tuple_cat(
-          std::make_tuple(std::tuple_cat(std::make_tuple(uint64_t{0}),
-                                         std::declval<FnAResult>()),
-                          std::tuple_cat(std::make_tuple(uint64_t{0}),
-                                         std::declval<FnBResult>())),
+          std::make_tuple(
+              std::declval<typename IsValidResult<FnAResult>::ResultTuple>(),
+              std::declval<typename IsValidResult<FnBResult>::ResultTuple>()),
           std::declval<GenResult>())));
-
-  static_assert(!IsFunctionType<FnA>::value,
-                "Timed functions should not be late-bound function types. Use "
-                "WRAP_FUNCTION.");
-  static_assert(!IsFunctionType<FnB>::value,
-                "Timed functions should not be late-bound function types. Use "
-                "WRAP_FUNCTION.");
 
  public:
   // Accumulate results for ~1 billion cycles worth of work.
@@ -240,8 +252,8 @@ class StatisticGenerator {
   // avoid giving too much weight to warm up artifacts early on.
   static constexpr uint64_t kMinObservations = 500;
 
-  explicit StatisticGenerator(Generator generator, PrepA prep_a, FnA fn_a,
-                              PrepB prep_b, FnB fn_b, Comparator comparator);
+  explicit StatisticGenerator(Generator generator, FnA fn_a, FnB fn_b,
+                              Comparator comparator);
 
   // (Re)creates internal worker threads.
   void Start(size_t num_threads);
@@ -258,19 +270,15 @@ class StatisticGenerator {
   // This struct provides a thread-private `Context` for each worker
   // thread that generates comparison results.
   struct Context {
-    explicit Context(Generator generator_, PrepA prep_a_, FnA fn_a_,
-                     PrepB prep_b_, FnB fn_b_, Comparator comparator_)
+    explicit Context(Generator generator_, FnA fn_a_, FnB fn_b_,
+                     Comparator comparator_)
         : generator(std::move(generator_)),
-          prep_a(std::move(prep_a_)),
           fn_a(std::move(fn_a_)),
-          prep_b(std::move(prep_b_)),
           fn_b(std::move(fn_b_)),
           comparator(std::move(comparator_)) {}
 
     Generator generator;
-    PrepA prep_a;
     FnA fn_a;
-    PrepB prep_b;
     FnB fn_b;
     Comparator comparator;
     xs256 prng;
@@ -311,37 +319,28 @@ class StatisticGenerator {
   Accumulator<Result> acc_;
 };
 
-template <typename Generator, typename PrepA, typename FnA, typename PrepB,
-          typename FnB, typename Comparator>
-constexpr size_t StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB,
-                                    Comparator>::kMaxBufferSize;
+template <typename Generator, typename FnA, typename FnB, typename Comparator>
+constexpr size_t
+    StatisticGenerator<Generator, FnA, FnB, Comparator>::kMaxBufferSize;
 
-template <typename Generator, typename PrepA, typename FnA, typename PrepB,
-          typename FnB, typename Comparator>
-constexpr uint64_t StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB,
-                                      Comparator>::kMinObservations;
+template <typename Generator, typename FnA, typename FnB, typename Comparator>
+constexpr uint64_t
+    StatisticGenerator<Generator, FnA, FnB, Comparator>::kMinObservations;
 
-template <typename Generator, typename PrepA, typename FnA, typename PrepB,
-          typename FnB, typename Comparator>
-constexpr uint64_t StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB,
-                                      Comparator>::kTargetCyclePerRun;
+template <typename Generator, typename FnA, typename FnB, typename Comparator>
+constexpr uint64_t
+    StatisticGenerator<Generator, FnA, FnB, Comparator>::kTargetCyclePerRun;
 
-template <typename Generator, typename PrepA, typename FnA, typename PrepB,
-          typename FnB, typename Comparator>
+template <typename Generator, typename FnA, typename FnB, typename Comparator>
 __attribute__((__noinline__))
-StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB,
-                   Comparator>::StatisticGenerator(Generator generator,
-                                                   PrepA prep_a, FnA fn_a,
-                                                   PrepB prep_b, FnB fn_b,
-                                                   Comparator comparator)
-    : context_(std::move(generator), std::move(prep_a), std::move(fn_a),
-               std::move(prep_b), std::move(fn_b), std::move(comparator)) {}
+StatisticGenerator<Generator, FnA, FnB, Comparator>::StatisticGenerator(
+    Generator generator, FnA fn_a, FnB fn_b, Comparator comparator)
+    : context_(std::move(generator), std::move(fn_a), std::move(fn_b),
+               std::move(comparator)) {}
 
-template <typename Generator, typename PrepA, typename FnA, typename PrepB,
-          typename FnB, typename Comparator>
+template <typename Generator, typename FnA, typename FnB, typename Comparator>
 __attribute__((__noinline__)) void
-StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB, Comparator>::Start(
-    size_t num_threads) {
+StatisticGenerator<Generator, FnA, FnB, Comparator>::Start(size_t num_threads) {
   workers_.clear();
   if (num_threads <= 1) {
     return;
@@ -353,10 +352,9 @@ StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB, Comparator>::Start(
   }
 }
 
-template <typename Generator, typename PrepA, typename FnA, typename PrepB,
-          typename FnB, typename Comparator>
+template <typename Generator, typename FnA, typename FnB, typename Comparator>
 __attribute__((__noinline__)) void
-StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB, Comparator>::Stop() {
+StatisticGenerator<Generator, FnA, FnB, Comparator>::Stop() {
   done_.Notify();
   for (auto& worker : workers_) {
     worker.join();
@@ -427,10 +425,9 @@ __attribute__((__noinline__)) uint64_t PublishResults(
   return ret;
 }
 
-template <typename Generator, typename PrepA, typename FnA, typename PrepB,
-          typename FnB, typename Comparator>
+template <typename Generator, typename FnA, typename FnB, typename Comparator>
 __attribute__((__noinline__)) auto
-StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB, Comparator>::Consume()
+StatisticGenerator<Generator, FnA, FnB, Comparator>::Consume()
     -> std::vector<std::vector<Result>> {
   using std::swap;
 
@@ -454,16 +451,9 @@ StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB, Comparator>::Consume()
   return ret;
 }
 
-template <typename Generator, typename PrepA, typename FnA, typename PrepB,
-          typename FnB, typename Comparator>
+template <typename Generator, typename FnA, typename FnB, typename Comparator>
 /*static*/ __attribute__((__noinline__)) void
-StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB, Comparator>::Work(
-    Context* context) {
-  // Warn about any suboptimal function type. Do so here instead of
-  // in WorkFn to warn once per template instantiation.
-  CopyFnTypeIfPossible<FnA, PrepAResult>::BadUsageNotice();
-  CopyFnTypeIfPossible<FnB, PrepBResult>::BadUsageNotice();
-
+StatisticGenerator<Generator, FnA, FnB, Comparator>::Work(Context* context) {
   // Populate an array with pointers to our semantically identical
   // instances of `WorkImpl` in order to randomly call one of them at
   // each iteration.  This should help balance consistent measurement
@@ -500,23 +490,13 @@ StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB, Comparator>::Work(
   }
 }
 
-template <typename Generator, typename PrepA, typename FnA, typename PrepB,
-          typename FnB, typename Comparator>
+template <typename Generator, typename FnA, typename FnB, typename Comparator>
 template <bool kCallAFirst>
-// Introduce extra alignment to avoid any consistent measurement error
-// introduced by mechanisms that work at extra wide granularity.  In
-// particular we want to make sure that fetching and decoding the code
-// does not affect specific instances of this function differently The
-// impact of wasted instruction bytes should be small (< 25%) compared
-// to the code size of this function.
-/*static*/ __attribute__((__aligned__(2 * ABSL_CACHELINE_SIZE), __noinline__))
-uint64_t
-StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB, Comparator>::WorkImpl(
+/*static*/ __attribute__((__noinline__)) uint64_t
+StatisticGenerator<Generator, FnA, FnB, Comparator>::WorkImpl(
     Context* context) {
-  using ResultsA = ConstructableArray<std::tuple<uint64_t, uint64_t, FnAResult>,
-                                      kResultsPerCall>;
-  using ResultsB = ConstructableArray<std::tuple<uint64_t, uint64_t, FnBResult>,
-                                      kResultsPerCall>;
+  using ResultsA = ConstructableArray<FnAResult, kResultsPerCall>;
+  using ResultsB = ConstructableArray<FnBResult, kResultsPerCall>;
   struct ResultAFirst {
     ResultsA a;
     ResultsB b;
@@ -537,56 +517,96 @@ StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB, Comparator>::WorkImpl(
   typename std::conditional<kCallAFirst, ResultAFirst, ResultBFirst>::type
       results;
 
-  typename CopyFnTypeIfPossible<FnA, PrepAResult>::type fn_a = context->fn_a;
-  typename CopyFnTypeIfPossible<FnB, PrepBResult>::type fn_b = context->fn_b;
-
-  // Generate one instance, and use it for one call to FnA and one
-  // call to FnB.
   auto work_unit = apply(context->generator, std::make_tuple());
-
-#define DO(X)                                              \
-  do {                                                     \
-    auto prep = apply(context->prep_##X, work_unit);       \
-                                                           \
-    const uint64_t begin = GetTicksBeginWithBarrier(prep); \
-    auto result = apply(fn_##X, std::move(prep));          \
-    if (std::tuple_size<decltype(result)>::value > 0) {    \
-      asm volatile("" ::"X"(result) : "memory");           \
-    }                                                      \
-    const uint64_t end = GetTicksEnd();                    \
-                                                           \
-    results.X.EmplaceAt(0, begin, end, std::move(result)); \
-  } while (0)
 
   SetupInterruptDetection();
 
+#if ABSL_CACHELINE_SIZE >= 128
+#define PAD_TIMING_CALL(FN) asm volatile(".align 128\n\t" : "+r"(FN)::"memory");
+#elif ABSL_CACHELINE_SIZE == 64
+#define PAD_TIMING_CALL(FN) asm volatile(".align 64\n\t" : "+r"(FN)::"memory");
+#else
+#define PAD_TIMING_CALL(FN) asm volatile(".align 32\n\t" : "+r"(FN)::"memory");
+#endif
+
   if (kCallAFirst) {
-    DO(a);
-    DO(b);
+    // Insert padding before each indirect call to make sure they get
+    // their own cache line, and thus prediction slot (hopefully).
+    auto* fn_a = &context->fn_a;
+    PAD_TIMING_CALL(fn_a);
+    results.a.EmplaceAt(0, (*fn_a)(&work_unit));
+    auto* fn_b = &context->fn_b;
+    PAD_TIMING_CALL(fn_b);
+    results.b.EmplaceAt(0, (*fn_b)(&work_unit));
     static_assert(kResultsPerCall == 1,
                   "kResultsPerCall must match number of results exactly.");
   } else {
     // Same thing, but mirrored.
-    DO(b);
-    DO(a);
+    auto* fn_b = &context->fn_b;
+    PAD_TIMING_CALL(fn_b);
+    results.b.EmplaceAt(0, (*fn_b)(&work_unit));
+    auto* fn_a = &context->fn_a;
+    PAD_TIMING_CALL(fn_a);
+    results.a.EmplaceAt(0, (*fn_a)(&work_unit));
     static_assert(kResultsPerCall == 1,
                   "kResultsPerCall must match number of results exactly.");
   }
-#undef DO
+
+#undef PAD_TIMING_CALL
 
   return PublishResults(std::move(work_unit), &results.a, &results.b,
                         &context->comparator, &context->buffer);
 }
 
-template <typename Generator, typename PrepA, typename FnA, typename PrepB,
-          typename FnB, typename Comparator>
+template <typename Generator, typename FnA, typename FnB, typename Comparator>
 /*static*/ __attribute__((__noinline__)) void
-StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB, Comparator>::WorkerFn(
+StatisticGenerator<Generator, FnA, FnB, Comparator>::WorkerFn(
     Context context, const absl::Notification* done, Accumulator<Result>* acc) {
   while (!done->HasBeenNotified()) {
     Work(&context);
     Flush(&context.buffer, acc);
   }
+}
+
+// Returns a function that applies `Prep` to its input and times a
+// call to `Fn` with the value returned by `Prep`.
+template <typename Generator, typename Prep, typename Fn,
+          typename GenResult = decltype(internal::CallAndTuplify()(
+              std::declval<Generator>(), std::make_tuple())),
+          typename PrepResult = decltype(internal::CallAndTuplify()(
+              std::declval<Prep>(), std::declval<const GenResult>())),
+          typename FnResult = decltype(internal::CallAndTuplify()(
+              std::declval<Fn>(), std::declval<PrepResult>())),
+          typename Result = std::tuple<uint64_t, uint64_t, FnResult>>
+std::function<Result(const GenResult*)> MakeTimingFunction(Prep prep_fn,
+                                                           Fn timed_fn) {
+  // Warn about any suboptimal function type.
+  internal::CopyFnTypeIfPossible<Fn, PrepResult>::BadUsageNotice();
+
+  // Force extra alignment to avoid any consistent measurement error
+  // introduced by mechanisms that work at extra wide granularity.  In
+  // particular we want to make sure that fetching and decoding the
+  // code does not affect specific instances of this function
+  // differently.
+  return [ prep_fn, timed_fn ](const GenResult* work_unit) __attribute__((
+      __aligned__(2 * ABSL_CACHELINE_SIZE), __noinline__)) mutable {
+    const internal::CallAndTuplify apply;
+
+    typename internal::CopyFnTypeIfPossible<Fn, PrepResult>::type local_fn =
+        timed_fn;
+
+    auto prep_input = apply(prep_fn, *work_unit);
+    const uint64_t begin = GetTicksBeginWithBarrier(prep_input);
+    auto result = apply(local_fn, std::move(prep_input));
+    // Force a compiler barrier on the result of the call, if it
+    // exists.
+    if (std::tuple_size<decltype(result)>::value > 0) {
+      asm volatile("" ::"X"(result) : "memory");
+    }
+    const uint64_t end = GetTicksEnd();
+
+    return std::make_tuple(begin, end, std::move(result));
+  };
 }
 }  // namespace internal
 
@@ -601,9 +621,15 @@ auto CompareFunctions(const TestParams& params, Generator generator,
   // to trigger side effects like warning in non-release builds.
   GetTicksOverhead();
 
-  internal::StatisticGenerator<Generator, PrepA, FnA, PrepB, FnB, Comparator>
-      stat_gen(std::move(generator), std::move(prep_a), std::move(fn_a),
-               std::move(prep_b), std::move(fn_b), std::move(comparator));
+  auto timing_function_a = internal::MakeTimingFunction<Generator>(
+      std::move(prep_a), std::move(fn_a));
+  auto timing_function_b = internal::MakeTimingFunction<Generator>(
+      std::move(prep_b), std::move(fn_b));
+
+  internal::StatisticGenerator<Generator, decltype(timing_function_a),
+                               decltype(timing_function_b), Comparator>
+      stat_gen(std::move(generator), std::move(timing_function_a),
+               std::move(timing_function_b), std::move(comparator));
 
   uint64_t num_comparisons = 0;
   const auto consume = [analysis, &stat_gen, &num_comparisons] {
