f73fc2eb7fac5b6b0d535c977d5ba0b562763b5b
Paul Khuong <pvk@pvk.ca>
2019-07-31 14:11:07 -0400

    bench/kolmogorov-smirnov-test: 3-way KS test
    This test can not only reject the hypothesis that A is never slower
    than B or vice versa, but can also determine when A is (probably)
    *always* faster than B, or vice versa, modulo some leeway.
    
    When A is always almost faster than B and B always almost faster than
    A, we can even declare approximate equality!
    
    Inversely, we can also determine that A is sometimes slower than B
    and A sometimes faster than B; they two are then different, but not
    comparable.
    
    TESTED=new unit and integration tests.

---
+++ bench/kolmogorov-smirnov-test_test.cc
@@ -0,0 +1,185 @@
+#include "kolmogorov-smirnov-test.h"
+
+#include <cstdint>
+#include <iostream>
+#include <random>
+
+#include "bench/test-params.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace bench {
+namespace {
+// Generate integers in U[0, 10]. We statistically can't detect strict
+// equality.
+TEST(KSTest, EqualTie) {
+  std::random_device dev;
+  std::mt19937 rng(dev());
+
+  std::uniform_int_distribution<uint64_t> u(0, 10);
+
+  KolmogorovSmirnovTest test(TestParams().SetMinEffect(0).SetMinCount(1000));
+  const auto comparator = test.comparator();
+  for (size_t i = 0; i < 1000; ++i) {
+    for (size_t j = 0; j < 1000; ++j) {
+      const std::pair<double, double> cmps[] = {
+          comparator(std::make_tuple(u(rng)), std::make_tuple(u(rng))),
+      };
+
+      test.Observe(cmps);
+    }
+
+    if (test.Done()) {
+      break;
+    }
+  }
+
+  EXPECT_FALSE(test.Done());
+  const auto result = test.Summary(&std::cout);
+  EXPECT_EQ(result.result, ComparisonResult::kInconclusive);
+}
+
+// But we can detect equality +/- 1.
+TEST(KSTest, Equal) {
+  std::random_device dev;
+  std::mt19937 rng(dev());
+
+  std::uniform_int_distribution<uint64_t> u(0, 10);
+
+  KolmogorovSmirnovTest test(
+      TestParams().SetMinEffect(1).SetMinDfEffect(1e-2).SetMinCount(1000));
+  const auto comparator = test.comparator();
+  for (size_t i = 0; i < 10000; ++i) {
+    for (size_t j = 0; j < 1000; ++j) {
+      const std::pair<double, double> cmps[] = {
+          comparator(std::make_tuple(u(rng)), std::make_tuple(u(rng))),
+      };
+
+      test.Observe(cmps);
+    }
+
+    if (test.Done()) {
+      break;
+    }
+  }
+
+  EXPECT_TRUE(test.Done());
+  const auto result = test.Summary(&std::cout);
+  EXPECT_EQ(result.result, ComparisonResult::kTie);
+}
+
+TEST(KSTest, EqualShiftedByOne) {
+  std::random_device dev;
+  std::mt19937 rng(dev());
+
+  std::uniform_int_distribution<uint64_t> u(0, 10);
+
+  KolmogorovSmirnovTest test(
+      TestParams().SetMinEffect(1 + 1e-6).SetMinDfEffect(1e-2).SetMinCount(
+          1000));
+  const auto comparator = test.comparator();
+  for (size_t i = 0; i < 10000; ++i) {
+    for (size_t j = 0; j < 1000; ++j) {
+      const std::pair<double, double> cmps[] = {
+          comparator(std::make_tuple(u(rng)), std::make_tuple(u(rng) + 1)),
+      };
+
+      test.Observe(cmps);
+    }
+
+    if (test.Done()) {
+      break;
+    }
+  }
+
+  EXPECT_TRUE(test.Done());
+  const auto result = test.Summary(&std::cout);
+  EXPECT_EQ(result.result, ComparisonResult::kTie);
+}
+
+TEST(KSTest, Lower) {
+  std::random_device dev;
+  std::mt19937 rng(dev());
+
+  std::uniform_int_distribution<uint64_t> u_a(0, 10);
+  std::uniform_int_distribution<uint64_t> u_b(0, 12);
+
+  KolmogorovSmirnovTest test(TestParams().SetMinEffect(1).SetMinCount(1000));
+  const auto comparator = test.comparator();
+  for (size_t i = 0; i < 1000; ++i) {
+    for (size_t j = 0; j < 1000; ++j) {
+      const std::pair<double, double> cmps[] = {
+          comparator(std::make_tuple(u_a(rng)), std::make_tuple(u_b(rng))),
+      };
+
+      test.Observe(cmps);
+    }
+
+    if (test.Done()) {
+      break;
+    }
+  }
+
+  const auto result = test.Summary(&std::cout);
+  EXPECT_EQ(result.result, ComparisonResult::kALower);
+}
+
+TEST(KSTest, Higher) {
+  std::random_device dev;
+  std::mt19937 rng(dev());
+
+  std::uniform_int_distribution<uint64_t> u_a(0, 12);
+  std::uniform_int_distribution<uint64_t> u_b(0, 10);
+
+  KolmogorovSmirnovTest test(
+      TestParams().SetMinEffect(1).SetMinDfEffect(1e-2).SetMinCount(1000));
+  const auto comparator = test.comparator();
+  for (size_t i = 0; i < 10000; ++i) {
+    for (size_t j = 0; j < 1000; ++j) {
+      const std::pair<double, double> cmps[] = {
+          comparator(std::make_tuple(u_a(rng)), std::make_tuple(u_b(rng))),
+      };
+
+      test.Observe(cmps);
+    }
+
+    if (test.Done()) {
+      break;
+    }
+  }
+
+  EXPECT_TRUE(test.Done());
+  const auto result = test.Summary(&std::cout);
+  EXPECT_EQ(result.result, ComparisonResult::kAHigher);
+}
+
+TEST(KSTest, Different) {
+  std::random_device dev;
+  std::mt19937 rng(dev());
+
+  std::uniform_int_distribution<uint64_t> u_a(0, 12);
+  std::uniform_int_distribution<uint64_t> u_b(2, 10);
+
+  KolmogorovSmirnovTest test(
+      TestParams().SetMinEffect(1).SetMinDfEffect(1e-2).SetMinCount(1000));
+  const auto comparator = test.comparator();
+  for (size_t i = 0; i < 10000; ++i) {
+    for (size_t j = 0; j < 1000; ++j) {
+      const std::pair<double, double> cmps[] = {
+          comparator(std::make_tuple(u_a(rng)), std::make_tuple(u_b(rng))),
+      };
+
+      test.Observe(cmps);
+    }
+
+    if (test.Done()) {
+      break;
+    }
+  }
+
+  EXPECT_TRUE(test.Done());
+  const auto result = test.Summary(&std::cout);
+  EXPECT_EQ(result.result, ComparisonResult::kDifferent);
+}
+}  // namespace
+}  // namespace bench
